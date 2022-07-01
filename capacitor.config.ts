69899fe20de62382e61d37c35e35f0a2b8f71240
Hiter Harris <hiterharris@comcast.net>
2022-07-01 13:30:31 -0400

    set up app, add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.urlshortener.hiterharris',
+  appName: 'URL Shortener',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
