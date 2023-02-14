c4f736dd40b9678c751c61642ac0b79c54fd5f16
Hiter Harris <hiterharris@comcast.net>
2023-02-14 13:58:47 -0500

    update for desktop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import useBitly from './hooks/useBitly';
 import { Input, Result } from './components';
+import { isDesktop } from 'react-device-detect';
 import './App.css';
 
 const App = () => {
   const [input, setInput] = useState('');
   const { getUrl, shortUrl, setShortUrl } = useBitly();
 
+  console.log('isDesktop: ', isDesktop)
+
   return (
-    <div className="App">
+    <div className={`App ${isDesktop ? 'desktop' : 'mobile'}`}>
       {!shortUrl ?
       <Input
         input={input}
         setInput={setInput}
         getUrl={getUrl}
+        isDesktop={isDesktop}
       /> :
       <Result
         input={input}
--- src/components/Input.js
+++ src/components/Input.js
@@ -2,17 +2,17 @@ import React from 'react';
 import '../App.css';
 
 const Input = (props) => {
-    const { input, setInput, getUrl } = props;
+    const { input, setInput, getUrl, isDesktop } = props;
 
     return (
-        <div className="Input">
+        <div className={`Input ${isDesktop ? 'desktop' : 'mobile'}`}>
             <input
                 type="text"
                 placeholder="Enter long URL..."
                 value={input}
                 onChange={e => setInput(e.target.value)}
             />
-            <button className="shorten-button" onClick={() => getUrl(input)}>Shorten URL</button>
+            <button className={`shorten-button ${isDesktop ? 'desktop' : 'mobile'}`} onClick={() => getUrl(input)}>Shorten URL</button>
         </div>
     );
 }
